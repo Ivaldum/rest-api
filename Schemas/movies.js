fa5b806079d4900b4696763828d6721312068ea8
Agustin Santos <agustinsantos63@gmail.com>
2024-03-16 12:29:41 -0300

    deploy API to FL0

---
+++ Schemas/movies.js
@@ -0,0 +1,31 @@
+const z = require('zod')
+
+const movieSchema = z.object({
+  title: z.string({
+    invalid_type_error: 'Movie tittle must be a string',
+    required_error: 'Movie tittle is required'
+  }),
+  year: z.number().int().min(1900).max(2024),
+  director: z.string(),
+  duration: z.number().int().positive(),
+  rate: z.number().min(0).max(10),
+  poster: z.string().url({
+    message: 'Poster must be a valid URL'
+  }),
+  genre: z.array(
+    z.enum(['Action', 'Adventure', 'Crime', 'Comedy', 'Drama', 'Fantase', 'Horror', 'Thriller', 'Sci-Fi'])
+  )
+})
+
+function validateMovie(object) {
+  return movieSchema.safeParse(object)
+}
+
+function validatePartialMovie(input) {
+  return movieSchema.partial().safeParse(input)
+}
+
+module.exports = {
+  validateMovie,
+  validatePartialMovie
+}
